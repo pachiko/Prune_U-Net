48049df1c991c20534e6f425467c7e61e435797c
kcang2 <42316301+kcang2@users.noreply.github.com>
2019-09-18 22:15:51 +0800

    util

--- utils/utils.py
+++ utils/utils.py
@@ -160,3 +160,30 @@ def get_save_dir(base_dir, name, training, id_max=100):
 
     raise RuntimeError('Too many save directories created with the same name. \
                        Delete old save directories or use another name.')
+
+class AverageMeter:
+    """Keep track of average values over time.
+
+    Adapted from:
+        > https://github.com/pytorch/examples/blob/master/imagenet/main.py
+    """
+    def __init__(self):
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def reset(self):
+        """Reset meter."""
+        self.__init__()
+
+    def update(self, val, num_samples=1):
+        """Update meter with new value `val`, the average of `num` samples.
+
+        Args:
+            val (float): Average value to update the meter with.
+            num_samples (int): Number of samples that were averaged to
+                produce `val`.
+        """
+        self.count += num_samples
+        self.sum += val * num_samples
+        self.avg = self.sum / self.count
